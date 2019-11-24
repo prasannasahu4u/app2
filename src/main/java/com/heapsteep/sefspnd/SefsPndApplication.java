35cd19f6513a7c7aec42e47d29588f4f409084b2
Prasanna <Prasanna@DESKTOP-D2G4QA6>
2019-11-24 22:45:46 +0530

    testing 222

---
+++ src/main/java/com/heapsteep/sefspnd/SefsPndApplication.java
@@ -0,0 +1,13 @@
+package com.heapsteep.sefspnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SefsPndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SefsPndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heapsteep/sefspnd/SefsPndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heapsteep.sefspnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SefsPndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
